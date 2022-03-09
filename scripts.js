da053d34baf646617189ca7987c54a1169fde1f4
gabriel-aires09 <aires514@gmail.com>
2022-03-09 17:50:20 -0300

    Exercicio n-1 resolvido

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+/* 1. Faça um algoritmo que calcule a soma de 50 a 
+* 100 usando a estrutura “for” e retorne no formato:
+* A soma de 50 a 100 é: X
+*/
+
+function sum () {
+  let soma = 0;
+  for (let index = 1; index <= 100; index += 1){
+    soma = soma + index; 
+  } console.log(soma)
+}
+sum();
+
 
 let contador = 0;
 
