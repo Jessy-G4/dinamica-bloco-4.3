615de2fda1fb97db1c498213a3339e8bbb1318cf
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2022-03-09 17:34:39 -0300

    Questão 2

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+let contador = 0;
+
+for (i = 0; i < 125; i++){
+    if (i % 3 == 0){
+        contador += 1;
+    }
+}
+
+console.log(contador);
+
+// 2. Crie um algoritmo que conte quantos números do intervalo entre 1 e 125 são divisíveis por 3.
+// Caso a quantidade seja igual a 50, exiba uma mensagem secreta a sua escolha.
