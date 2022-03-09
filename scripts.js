967ea22e64ec6e4b1fb2657d754b47861e641451
gabriel-aires09 <aires514@gmail.com>
2022-03-09 18:05:46 -0300

    Retirando o exercicio do bruno para nao ter conflito branch

--- scripts.js
+++ scripts.js
@@ -13,15 +13,4 @@ function sum () {
 sum();
 
 
-/* 2. Crie um algoritmo que conte quantos números do intervalo entre 1 e 125 são divisíveis por 3.
- * Caso a quantidade seja igual a 50, exiba uma mensagem secreta a sua escolha.
- */
-
-let contador = 0;
-
-for (i = 0; i < 125; i++){
-    if (i % 3 == 0){
-        contador += 1;
-    }
-} console.log(contador);
 
