9e2847f7f878f88a85aa99184c1cc6ded6b996d0
Jessy Cristina <jessyaraujo395@gmail.com>
2022-03-09 18:28:38 -0300

    questão 5

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// 5. Crie um algoritmo que recebe a idade de Marcella, Carlos e Camila e imprima quem é a mais nova no formato:
+
+let Pessoas = ["Marcela", "Carlos", "Camila"];
+let MarcelaIdade = 13;
+let CarlosIdade = 23;
+let CamilaIdade = 103;
+
+if(MarcelaIdade < CamilaIdade && CarlosIdade) {
+    console.log("Marcela é a mais nova")
+} else if(CamilaIdade < MarcelaIdade && CarlosIdade) {
+    console.log("Camila é a mais nova")
+} else if(CarlosIdade < MarcelaIdade && CamilaIdade) {
+    console.log("Carlos é a mais nova")
+}
